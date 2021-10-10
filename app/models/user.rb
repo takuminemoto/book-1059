51738e77ef1e2ecb966a76f3cdc9efd61254dbff
takuminemoto <nemoto1671055@gmail.com>
2021-10-10 08:03:56 +0000

    string name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+#database_authenticatable（パスワードの正確性を検証）
+#registerable（ユーザ登録や編集、削除）
+#recoverable（パスワードをリセット）
+#rememberable（ログイン情報を保存）
+#validatable（emailのフォーマットなどのバリデーション）
+
