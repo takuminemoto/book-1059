7dbf87c737c67e3d5a9e6338972567cb3645d9dc
takuminemoto <nemoto1671055@gmail.com>
2021-10-20 08:44:40 +0000

    10/20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
     @users = User.all
     @user = current_user
   end
-  
+
   def edit
     @user = User.find(params[:id])
     if @user.id == current_user.id
@@ -23,13 +23,13 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user.id)
     end
   end
-  
+
   def create
     user = User.new(user_params)
     user.save
     redirect_to '/top'
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -38,20 +38,20 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :image)
   end
-  
+
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
     end
   end
-  
+
 end
 
 
